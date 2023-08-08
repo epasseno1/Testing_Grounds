61c8cbe4a91abbf9b3ab423ce9d4eda4c7119487
Eric <epasseno@mail.walshcollege.ed>
2023-08-08 12:47:14 -0400

    Integrated file search function into main code.

--- Diff_Tool/Readme.txt
+++ Diff_Tool/Readme.txt
@@ -26,8 +26,18 @@ clear text.
 
 
 
+I need to introduce logic and controls for the file search function that will allow 
+for remediation in the event that a file is not found, or there are duplicate files.
+
+This will require an input validation like function to serve as a loop 
 
 
+
+
+I will use the file locater function to get the input from the user on the file they want to use
+Then, the file object locater will take that information, perform a search, then if the 
+file was found, return false and no file path. If the search succeeds, then return true, and the 
+absolute path for the object. 
 
 
 
--- Diff_Tool/diff_tool_functions.py
+++ Diff_Tool/diff_tool_functions.py
@@ -1,23 +1,44 @@
+import zipfile, os
+import platform
+import os
+import shutil
+from pathlib import Path
 
-from platform import *
 
+
+
+# def file_import():
+"""Will be used for input validation and looping, but main logic needs work still. """
+#     """A function that can be used to import files."""
+#     tmp_file = file_locator()
+#     print(tmp_file)
 
 
 def file_import():
-    """A function that can be used to import files."""
-    print("I worked")
-
-
-def file_locator():
     """A function that can be used to locate files on the C: drive and copy their ABS or REL path to the clipboard."""
+    #found = False
+    tmp_ObType= input("Is the item you are searching for folder (1) or file (2)? ")
+    tmp_ObName = input("Please enter the name of the object you wish to locate.")
+    tmp_file_found = fileObjectLocator(tmp_ObName, int(tmp_ObType))
+    print(tmp_file_found)
+
+    
+def fileObjectLocator(temp_fileN, temp_type):
+    root = getPath()
+    my_return = os.walk(root)
+
+    for item in my_return:
+        for filename in item[temp_type]:
+            if filename == temp_fileN:
+                location = (os.path.join(item[0], filename))
+                return location
 
 
-def file_find():
-    """A function that is used to locate files within the FS of the system"""
 
 
 def getPath():
-    """A function that will be used to determine the OS of the host system"""
+    """A function that will be used to determine the OS of the host system which is used for file lookups
+    """
     my_system = platform.system() 
 
     if my_system == "Windows":
@@ -25,3 +46,5 @@ def getPath():
     else:
         root_fs ="/"
     return root_fs
+
+

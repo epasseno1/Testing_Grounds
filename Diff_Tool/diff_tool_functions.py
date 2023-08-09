878eb8c23883f8928594c0fcfe4139a161acafc5
Eric <epasseno@mail.walshcollege.ed>
2023-08-09 08:42:22 -0400

    update

--- Diff_Tool/diff_tool_functions.py
+++ Diff_Tool/diff_tool_functions.py
@@ -14,10 +14,18 @@ from pathlib import Path
 #     print(tmp_file)
 
 
-def file_import():
-    """A function that can be used to locate files on the C: drive and copy their ABS or REL path to the clipboard."""
+
+def get_obj_name():
+    """A function that is used to capture the name of an object or file, and pass it to a validator"""
     tmp_ObType= input("Is the item you are searching for folder (1) or file (2)? ")
     tmp_ObName = input("Please enter the name of the object you wish to locate.")
+    # Here I need to start the while false loop and send the input over to the validator
+    # Things to validate for obj type are that the input is nothing but numbers, and not empty
+    # things to validate for the file name; ban special characters, limit length of filename, no blank values. 
+
+
+def file_import():
+    """A function that can be used to locate files on the C: drive and copy their ABS or REL path to the clipboard."""
     tmp_file_found = fileObjectLocator(tmp_ObName, int(tmp_ObType))
     print(tmp_file_found)
 

5e4946c2778a05dae2a0e1f24e261724eceaff53
Eric <epasseno@mail.walshcollege.ed>
2023-08-11 15:39:47 -0400

    Began learning tkinter to start building the GUI

---
+++ Diff_Tool/diff_tool_gui.py
@@ -0,0 +1,71 @@
+import tkinter as tk
+
+
+# # This can be used to create the window that will eventually contain items for the applicaiton
+# root = tk.Tk()
+# root.mainloop()
+
+
+
+root = tk.Tk()
+
+## This can be used to adjust the initial size of the window that is opened when program is 
+# launched. 
+root.geometry("800x400")
+
+# This is used to add a title to the window that is opened with the program
+root.title("diffcheck.exe")
+
+# This is used to add text to the GUI
+
+# labels require a parent object, in this case, I will use the root itself as the parent.
+label = tk.Label(root, text="Hello", font=('arial', 18))
+
+# in the above, the parent object is the main window (root), Text is self explanitory, 
+# and the fony uses the font type, and then the size of the font
+
+# in order to 
+
+
+# Stopped at 5:21 - https://www.youtube.com/watch?v=ibf5cx221hk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# This value needs to remain at the bottom of the screen, sort of like HTML
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
